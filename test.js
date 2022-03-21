1c4427a34d7ea48ccee67f8326167aebf260923a
hayrapetyanhasmik <hasyokahasiko7@gmail.com>
2022-03-22 01:27:17 +0400

    Added 4 Mocha hooks

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-let assert = require("assert");
+/*let assert = require("assert");
 describe("Mathematical Operations", function(){
     it("Addition of two numbers", function(){
           let a = 10;
@@ -27,4 +27,27 @@ it("Division of two numbers", function(){
     let c = a/b;
     assert.equal(c,1);
 });
+});*/
+
+describe('Mocha Hooks', function(){
+
+    before('Execute Before All Tests', function(){
+        console.log('Execute Before All Tests');
+    });
+
+    beforeEach('Execute Before Each Test', function(){
+        console.log('Execute Before Each Test');
+    });
+
+    after('Execute After All Tests', function(){
+        console.log('Execute After All Tests');
+    });
+
+    afterEach('Execute After Each Test', function(){
+        console.log('Execute After Each Test');
+    });
+
+    it('Mocha Hooks Test', function(){
+        console.log('Mocha - This is a Test for Mocha Hooks');
+    });
 });
