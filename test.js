83a94eb33887a31ec9cde9160a701d4ac0665ed2
hayrapetyanhasmik <hasyokahasiko7@gmail.com>
2022-03-21 16:14:20 +0400

    Added test.js

---
+++ test.js
@@ -0,0 +1,30 @@
+let assert = require("assert");
+describe("Mathematical Operations", function(){
+    it("Addition of two numbers", function(){
+          let a = 10;
+          let b = 10;
+          let c = a+b;
+          assert.equal(c,20);
+    });
+    
+    it("Subtraction of two numbers", function(){
+        let a = 10;
+        let b = 10;
+        let c = a-b;
+        assert.equal(c,0);
+  });
+
+  it("Multiplication of two numbers", function(){
+    let a = 10;
+    let b = 10;
+    let c = a*b;
+    assert.equal(c,100);
+});
+
+it("Division of two numbers", function(){
+    let a = 10;
+    let b = 10;
+    let c = a/b;
+    assert.equal(c,1);
+});
+});
