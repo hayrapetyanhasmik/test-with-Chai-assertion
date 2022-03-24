b8020303e3d4ec2bca30772335fe02df2c0db237
hayrapetyanhasmik <hasyokahasiko7@gmail.com>
2022-03-25 01:34:39 +0400

    Using  - should-

--- test.js
+++ test.js
@@ -1,8 +1,13 @@
 const chai = require("chai");
+
+//expect
 const expect = chai.expect;
+//should()
+const should = chai.should();
+
 let a = 1, b = 1;
 expect(a).to.be.equals(b);
-
+(a).should.be.equals(b);
 
 function myObj(){
     return{
@@ -12,12 +17,19 @@ function myObj(){
 }
 x = new myObj(), y = new myObj();
 expect(x).to.be.an("object");
-//expect(x).to.be.equals(y, "x and y are not equal");
 
 expect(x).to.be.deep.equals(y);
 
+(x).should.be.deep.equals(y);
+
 expect(x).to.be.an("object").and.to.be.deep.equals(y);
+
+(x).should.be.an("object").and.to.be.deep.equals(y);
 
 //arrays
 let arr = [1,2,3,4];
-expect(arr).to.be.an("array").that.includes(0);
+expect(arr).to.be.an("array").that.includes(1);
+
+(arr).should.be.an("array").that.includes(1);
+
+
