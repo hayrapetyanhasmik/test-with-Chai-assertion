20d3c4bbe25b51d6b66ec8d3a6930a3d6407556f
hayrapetyanhasmik <hasyokahasiko7@gmail.com>
2022-03-25 01:45:30 +0400

    Using -assert-

--- test.js
+++ test.js
@@ -4,10 +4,13 @@ const chai = require("chai");
 const expect = chai.expect;
 //should()
 const should = chai.should();
+//assert - TDD
+const assert = chai.assert; 
 
 let a = 1, b = 1;
 expect(a).to.be.equals(b);
 (a).should.be.equals(b);
+assert.equal(a,b);
 
 function myObj(){
     return{
@@ -21,6 +24,7 @@ expect(x).to.be.an("object");
 expect(x).to.be.deep.equals(y);
 
 (x).should.be.deep.equals(y);
+assert.deepEqual(x,y);
 
 expect(x).to.be.an("object").and.to.be.deep.equals(y);
 
@@ -31,5 +35,5 @@ let arr = [1,2,3,4];
 expect(arr).to.be.an("array").that.includes(1);
 
 (arr).should.be.an("array").that.includes(1);
-
+assert.isArray(arr);
 
