f751bb12fedc83b0cf1eedd34a92cd3e193ed6a7
hayrapetyanhasmik <hasyokahasiko7@gmail.com>
2022-03-25 00:54:40 +0400

    Using expect in objects / passing test

--- test.js
+++ test.js
@@ -3,3 +3,12 @@ const expect = chai.expect;
 let a = 1, b = 1;
 expect(a).to.be.equals(b);
 
+
+function myObj(){
+    return{
+        a: 100,
+        b: "Hello"
+    }
+}
+x = new myObj(), y = new myObj();
+expect(x).to.be.an("object");
